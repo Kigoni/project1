697bc1df783d5029c9562c1de18cac897b9a2e0b
Kigoni <kigonimike5@gmail.com>
2025-07-17 15:53:25 +0300

    2nd one

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          charts: ['chart.js'],
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
